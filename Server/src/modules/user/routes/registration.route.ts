19f26ac822290a6877edde4aec8867ff143ccc9b
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-01-27 23:28:17 +0100

    Update registration.route.ts

--- Server/src/modules/user/routes/registration.route.ts
+++ Server/src/modules/user/routes/registration.route.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from 'express';
-import userController from '../controllers/register.user';
-import { TypedRequest, UserResponse, ServiceResponse } from '../types/type';
+
+import userRegistration from '../controllers/register.user';
+import userController from '../controllers/user.controller';
+import { TypedRequest } from '../types/type';
+import { authorise } from '../../../middlewares/authorisation';
+import { UserRole } from '../../../utils/roles';
 
 
 const userRouter = Router();
@@ -8,12 +12,12 @@ const userRouter = Router();
 userRouter.get('/health', (req: Request, res: Response) => {
   res.status(200).json({ message: "Healthy!"})
 })
-userRouter.post("/register",  async (req: TypedRequest, res: Response) => {
-  await userController.create(req, res);
+userRouter.post("/register", async (req: TypedRequest, res: Response) => {
+  await userRegistration.register(req, res);
 });
 
 userRouter.post("/verify", async (req: Request, res: Response) => {
-  await userController.verifyUser(req, res);
+  await userRegistration.verifyUser(req, res);
 });
 
 userRouter.get('/all', async (req: Request, res: Response) => {
