ceb33e5624dd84e23bc52cc5a9ab083d56f4cff0
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-01-27 23:29:22 +0100

    Refactor user registration controller to improve structure and remove unused methods

--- Server/src/modules/user/controllers/register.user.ts
+++ Server/src/modules/user/controllers/register.user.ts
@@ -1,13 +1,13 @@
 import { Request as ExpressRequest, Response } from 'express';
 import * as yup from 'yup';
 
-import { registerUser, getAllUsers, getOneUser, updateUser, verifyUser } from '../services/user.registration';
-import { userRegistrationSchema, userUpdateSchema, userVerificationSchema } from '../../../utils/validator';
-import { TypedRequest, UserData, UserResponse, UserResponseData,  } from '../types/type';
+import { verifyUser, registerUser } from '../services/user.registration';
+import { userRegistrationSchema, userVerificationSchema } from '../../../utils/validator';
+import { TypedRequest, UserData, UserResponse, VerifyRequest } from '../types/type';
 
 
-const userController = {
-  create: async (req: TypedRequest, res: Response): Promise<Response> => {
+const userRegistration = {
+  register: async (req: TypedRequest, res: Response): Promise<Response> => {
       try {
         const validatedData = await userRegistrationSchema.validate(req.body, { 
           abortEarly: false 
@@ -37,65 +37,16 @@ const userController = {
       }
   },
 
-  verifyUser: async (req: ExpressRequest, res: Response): Promise<Response> => {
+  verifyUser: async (req: VerifyRequest, res: Response): Promise<Response> => {
     try {
       const verify = userVerificationSchema.validate(req.body, { abortEarly: false });
-      console.log('verify');
-      const {email, code } = verify as unknown as { email: string, code: string };
-      const user = await verifyUser(email, code);
+      
+      const { email, code } = verify as unknown as { email: string, code: string };
+      const verifyRequest: VerifyRequest = {
+        body: { email, code },
+      } as VerifyRequest;
+      const user = await verifyUser(verifyRequest);
       return res.status(user.statusCode).send({ status: (user.status), message: (user.message), data: (user.data)})
-    } catch (error) {
-      return res.status(500).send({
-        error: error
-      })
-    }
-  },
-
-  getAllUsers: async (req: ExpressRequest, res: Response): Promise<Response> => {
-    try {
-      const users: UserResponseData = await getAllUsers()
-      console.log(users);
-      return res.status(users.statusCode).send({
-        status: users.status,
-        message: users.message,
-        data: users.data
-      });
-    } catch (error) {
-      const errorResponse: UserResponseData = {
-        statusCode: 500,
-        status: "error",
-        message: "internal server error",
-        data: null
-      };
-
-      return res.status(errorResponse.statusCode).json({
-        status: errorResponse.status,
-        message: errorResponse.message,
-        data: errorResponse.data
-      });
-      }
-    
-  },
-
-  getOneUser: async (req: ExpressRequest, res: Response): Promise<Response> => {
-    try {
-      const user = await getOneUser(req.params.id);
-      return res.status(user.statusCode).send({ status: (user.status), message: (user.message), data: (user.data)})
-    } catch (error) {
-      return res.status(500).send({
-        error: error
-      })
-    }
-  },
-
-  updateUser: async (req: ExpressRequest, res: Response): Promise<Response> => {
-    try {
-      const id = req.params.id;
-      const validatedData = await userUpdateSchema.validate(req.body, { 
-        abortEarly: false 
-      });
-      const update = await updateUser( id, validatedData);
-      return res.status(update.statusCode).send({ status: (update.status), message: (update.message), data: (update.data)})
     } catch (error) {
       return res.status(500).send({
         error: error
@@ -104,4 +55,4 @@ const userController = {
   },
 };
 
-export default userController;
+export default userRegistration;
