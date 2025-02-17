7bd4e796259351a3faa78011ce4ba4546b1ec03b
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-17 20:32:59 +0100

    Refactor router to update module imports and add new routes for hospital, appointment, doctor, and email verification

--- Server/src/router.ts
+++ Server/src/router.ts
@@ -1,13 +1,19 @@
 import express, { Router } from "express";
 
-import userRouter from "./modules/user/routes/registration.route";
+import userRouter from "./modules/user/routes/user.route";
 import loginRouter from "./modules/user/routes/login.route";
-import providerRouter from "./modules/provider/routes/provider.route";
+import hospitalRouter from "./modules/hospital/routes/hospital.route";
+import appointmentRouter from "./modules/appointment/routes/appointment.route";
+import doctorRouter from "./modules/doctor/routes/doctor.route";
+import verifyRouter from "./modules/commonRoute/verify.email";
 
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/log", loginRouter);
-router.use("/provider", providerRouter);
+router.use('/verify', verifyRouter);
+router.use("/hospital", hospitalRouter);
+router.use('/appointment', appointmentRouter);
+router.use('/doctor', doctorRouter);
 
 export default router;
